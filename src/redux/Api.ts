fa3e2ab865af5668b3cb5f099bf3ba38c3a6a4e1
Alex Arendsen <alex.arendsen.7@gmail.com>
2023-04-19 19:34:52 -0400

    Flip back to hosted API

--- src/redux/Api.ts
+++ src/redux/Api.ts
@@ -2,5 +2,6 @@ import { ApiClient } from "../utils/ApiClient";
 
 const Legacy =  new ApiClient('Legacy NuList API', 'https://nulist.app/api');
 const Local =  new ApiClient('Local Trove API', 'http://192.168.0.20:8118/api');
+const Trove2023 =  new ApiClient('Trove API', 'https://nulist.app/api');
 
-export const Api = Local
+export const Api = Trove2023
