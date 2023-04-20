424586bf7135c892f6f3bac17623a2eb206e719e
Alex Arendsen <alex.arendsen.7@gmail.com>
2023-04-19 20:09:18 -0400

    Use alt API domain for now

--- src/redux/Api.ts
+++ src/redux/Api.ts
@@ -3,5 +3,6 @@ import { ApiClient } from "../utils/ApiClient";
 const Legacy =  new ApiClient('Legacy NuList API', 'https://nulist.app/api');
 const Local =  new ApiClient('Local Trove API', 'http://192.168.0.20:8118/api');
 const Trove2023 =  new ApiClient('Trove API', 'https://nulist.app/api');
+const Trove2023Alt =  new ApiClient('Trove API', 'https://trove-api-n5wur.ondigitalocean.app');
 
-export const Api = Trove2023
+export const Api = Trove2023Alt
